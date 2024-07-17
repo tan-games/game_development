3fa334d650918cc2ac7753a63affb3057e680096
myjju4208 <anyjuice@outlook.kr>
2024-07-17 15:16:40 +0900

    플레이어
     - 플레이어 클래스 정의
     - Player Class 개발 이슈 구현함

--- source/entity.py
+++ source/entity.py
@@ -9,5 +9,34 @@ window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 class Rock(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("../resource/images/rock.png")
+        self.image = pygame.image.load("../resource/items/rock.png")
         self.rect = self.image.get_rect()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("../resource/player.png")
+        self.rect = self.image.get_rect()
+        self.health = 3
+        self.vel = 0
+
+        self.rect.centerx = 600
+        self.rect.bottom = WINDOW_HEIGHT
+
+    def update(self):
+        self.rect.y += self.vel
+        if self.rect.bottom <= WINDOW_HEIGHT:
+            self.vel += 2
+        if self.rect.bottom > WINDOW_HEIGHT:
+            self.rect.bottom = WINDOW_HEIGHT
+            self.vel = 0
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_s]:
+            pass
+        if keys[pygame.K_d] and (self.rect.bottom == WINDOW_HEIGHT):
+            self.vel -= 35
+
+
+player = Player()
